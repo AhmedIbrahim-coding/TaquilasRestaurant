f8b9dc23a510a5598ac3e5cf67c632aaba66f65d
Ahmed_Eladawy <ahmed.ibrahim.eladawy7@gmail.com>
2026-09-04 15:18:38 +0300

    Hello world

---
+++ TaquilasRestaurant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaquilasRestaurant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
