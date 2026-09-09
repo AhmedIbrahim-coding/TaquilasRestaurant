04f5eec49ec4026696c849618dc15422de8197d5
Ahmed_Eladawy <ahmed.ibrahim.eladawy7@gmail.com>
2026-09-09 11:49:51 +0300

    Added generic repository

---
+++ TaquilasRestaurant/Models/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TaquilasRestaurant.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetById(int id, QueryOptions<T> options);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+    }
+}
---
+++ TaquilasRestaurant/Models/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaquilasRestaurant.Data;
+
+namespace TaquilasRestaurant.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected ApplicationDbContext _context { get; set; }
+        private DbSet<T> _dbSet {  get; set; }
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id, QueryOptions<T> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
